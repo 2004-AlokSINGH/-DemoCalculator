a40e6656d636687f5592d22de217d934f77a3e96
ALOK SINGH <123860933+2004-AlokSINGH@users.noreply.github.com>
2025-04-01 00:32:19 +0530

    Update JenkCalciTest.java

--- src/test/java/JenkCalciTest.java
+++ src/test/java/JenkCalciTest.java
@@ -1,15 +1,32 @@
+// import org.testng.Assert;
+// import org.testng.annotations.Test;
+
+// public class JenkCalciTest {
+
+//     @Test
+//     public void test1(){
+//         Assert.assertEquals(JenkCalci.add(3,3),6);
+//     }
+
+//     @Test
+//     public void test2(){
+//         Assert.assertEquals(JenkCalci.add(4,3),7);
+//     }
+// }
+
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class JenkCalciTest {
 
     @Test
-    public void test1(){
-        Assert.assertEquals(JenkCalci.add(3,3),6);
+    public void test1() {
+        Assert.assertEquals(JenkCalci.add(3, 3), 6);
     }
 
     @Test
-    public void test2(){
-        Assert.assertEquals(JenkCalci.add(4,3),7);
+    public void test2() {
+        Assert.assertEquals(JenkCalci.add(4, 3), 7);
     }
 }
